614a650c020a4681d039f3df7b8cd108094ebd5b
Franco Carrizo <franco.carrizo@bitlogic.io>
2022-06-25 18:38:01 -0300

    add redux rtk

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import StrapiApolloProvider from '../graphql/apollo'
+import { store as ReduxStore } from '../store/store'
+import { Provider as ReduxProvider } from 'react-redux'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <StrapiApolloProvider>
-      <Component {...pageProps} />
-    </StrapiApolloProvider>
+    <ReduxProvider store={ReduxStore}>
+      <StrapiApolloProvider>
+        <Component {...pageProps} />
+      </StrapiApolloProvider>
+    </ReduxProvider>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,18 @@ import { useQuery } from '@apollo/client'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useSelector, useDispatch } from 'react-redux'
+import { increment, decrement } from '../features/counter/counterSlice'
 import { UserDocument } from '../generated'
+import { RootState } from '../store/store'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   const { data, error, loading } = useQuery(UserDocument)
+
+  const count = useSelector((state: RootState) => state.counter.value)
+  console.log(count)
+  const dispatch = useDispatch()
   console.log(data)
   return (
     <div className={styles.container}>
@@ -16,7 +23,19 @@ const Home: NextPage = () => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <div>
+          <div>
+            <button aria-label='Increment value' onClick={() => dispatch(increment())}>
+              Increment
+            </button>
+            <span>{count}</span>
+            <button aria-label='Decrement value' onClick={() => dispatch(decrement())}>
+              Decrement
+            </button>
+          </div>
+        </div>
+      </main>
 
       <footer className={styles.footer}>
         <a
