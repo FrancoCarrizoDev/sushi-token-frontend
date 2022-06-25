d9207e0421cbe868f692161ea5b08f49580efc82
Franco Carrizo <franco.carrizo@bitlogic.io>
2022-06-25 01:00:49 -0300

    first commit, init proyect with graphql!

---
+++ graphql/apollo.tsx
@@ -0,0 +1,18 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import React, { FC, ReactNode } from 'react'
+
+const GRAPHQL_ENDPOINT = 'http://localhost:1337/graphql'
+
+interface Props {
+  children: ReactNode
+}
+
+const StrapiApolloProvider: FC<Props> = ({ children }) => {
+  const client = new ApolloClient({
+    uri: GRAPHQL_ENDPOINT,
+    cache: new InMemoryCache()
+  })
+  return <ApolloProvider client={client}>{children}</ApolloProvider>
+}
+
+export default StrapiApolloProvider
